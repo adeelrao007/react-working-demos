f4fdc7b6c8c95f1a96078925327ee66d47fd785e
Adeel Ahmad Rao <adeelrao007@gmail.com>
2026-02-03 07:45:08 +0500

    theme into folder and assets imported

---
+++ realestate/single-property/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './assets/css/bootstrap.min.css';
+import './assets/css/plugins.css';
+import './assets/css/swiper.css';
+import './assets/css/style.css';
+import './assets/css/coloring.css';
+import './assets/css/swiper-custom-1.css';
+import './assets/css/colors/scheme-01.css';
+import './assets/css/datepicker.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* Template content will be added here as React components */}
+      <h1>Single Property Real Estate Template (React)</h1>
+      <p>CSS assets from the template have been imported. Start building components!</p>
+    </div>
+  );
+}
+
+export default App;
